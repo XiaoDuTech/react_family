740504a2b1dafb71618c540a8d8b081db7bfc5ce
D <andy.du@ian-soft.com>
2018-02-16 22:19:28 +0800

    添加react-router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Hello from './component/Hello/Hello'
+
+import getRouter from './router/router';
+
 ReactDom.render(
-  <Hello/>,
+  getRouter(),
   document.getElementById('app')
 );
