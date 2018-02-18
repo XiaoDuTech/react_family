d90976d0d0b56860ba7fd2cb64367aed7830e974
D <andy.du@ian-soft.com>
2018-02-18 20:41:12 +0800

    按需加载（懒加载）

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -8,7 +8,8 @@ module.exports = {
   /*输出到dist文件夹，输出文件名字为bundle.js*/
   output:{
     path:path.join(__dirname,'../dist'),
-    filename:'bundle.js'
+    filename:'bundle.js',
+    chunkFilename:'[name].js'
   },
   /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
   /*cacheDirectory使用来缓存编译结果，下次编译加速*/
--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
 
-import Home from 'pages/Home/Home';
-import Page1 from 'pages/Page1/Page1';
-import Counter from 'pages/Counter/Counter';
-import UserInfo from 'pages/UserInfo/UserInfo';
+import Bundle from './Bundle';
+
+import Home from 'bundle-loader?lazy&name=home!pages/Home/Home';
+import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/Page1';
+import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/Counter';
+import UserInfo from 'bundle-loader?lazy&name=userInfo!pages/UserInfo/UserInfo';
+
+const Loading = function () {
+  return <div>Loading...</div>
+};
+
+const createComponent = (component)=>(props)=>(
+  <Bundle load = {component}>
+    {
+      (Component)=>Component ? <Component {...props}/> : <Loading />
+    }
+  </Bundle>
+);
 
 const getRouter = () => (
   <Router>
@@ -17,10 +31,10 @@ const getRouter = () => (
         <li><Link to="/userinfo">UserInfo</Link></li>
       </ul>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/page1" component={Page1}/>
-        <Route path="/counter" component={Counter}/>
-        <Route path="/userinfo" component={UserInfo}/>
+        <Route exact path="/" component={createComponent(Home)}/>
+        <Route path="/page1" component={createComponent(Page1)}/>
+        <Route path="/counter" component={createComponent(Counter)}/>
+        <Route path="/userinfo" component={createComponent(UserInfo)}/>
       </Switch>
     </div>
   </Router>
