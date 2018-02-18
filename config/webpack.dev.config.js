2724607a1a39265d3647caa2601a4f59a162d701
D <andy.du@ian-soft.com>
2018-02-18 18:12:51 +0800

    devtool优化

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -33,5 +33,6 @@ module.exports = {
       actions:path.join(__dirname,'../src/redux/actions'),
       reducers:path.join(__dirname,'../src/redux/reducers')
     }
-  }
+  },
+  devtool:'inline-source-map'
 }
